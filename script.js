046acc8d50be84c9e59d966baa00ad98b871cb58
Abdu <33095489+Tittou10@users.noreply.github.com>
2022-12-20 13:15:15 +0100

    Done Black and random color for each button.

--- script.js
+++ script.js
@@ -2,20 +2,19 @@
 
 */
 
+let startColor='';
 const container=document.querySelector('.container');
 
 const grid=document.createElement('div');
 grid.className='grid';
 document.querySelector('.container').appendChild(grid);
-const btn=document.querySelector('button');
+const btn=document.querySelector('.input');
 var input = document.createElement("input");
 input.type = "number";
 input.className = "inputNum";
 document.body.appendChild(input);
 
-//input.addEventListener('input', createSquare);
 btn.addEventListener('click', createSquare);
-let rainBow=document.getElementById('check');
 
 
 
@@ -28,20 +27,14 @@ function removeAllChildNodes(parent) {
    }
 }
 
-function changeColor(event) {
-   event.target.style.backgroundColor = randomColor();
-}
-
-
-function randomColor() {
-   var randomRed = Math.floor(Math.random() * 255);
-   var randomGreen = Math.floor(Math.random() * 255);
-   var randomBlue = Math.floor(Math.random() * 255);
-   //create the string that is the ‘random color’
-   var randomColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
-
-   return randomColor;
-}
+function getRandomRgb() {
+   var num = Math.round(0xffffff * Math.random());
+   var r = num >> 16;
+   var g = num >> 8 & 255;
+   var b = num & 255;
+   return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+ }
+ 
 
 let range=0;
 function createSquare(e){
@@ -50,7 +43,6 @@ function createSquare(e){
     let inp= prompt("please entre the Grid size max 100!");  
 
     console.log( e);
-    //range= e.target.valueAsNumber;
     range= parseInt(inp);
     console.log(typeof range);
 
@@ -59,65 +51,40 @@ function createSquare(e){
  
     sqr.className='sqr';
     sqr.id=`${index}`;
-
-    rainBow.addEventListener('click',()=>
-{
-   sqr.addEventListener('click', changeColor);
-});
-    sqr.addEventListener('click', (e)=>{
-      sqr.style.background=document.getElementById('favcolor1').value;})
-  
-  
-  
+    sqr.addEventListener('mouseover', colorize);
 
     grid.appendChild(sqr);
     grid.style.gridTemplateColumns =`repeat(${range}, 1fr)`;
     grid.style.gridTemplateRows =`repeat(${range}, 1fr)`;;
 
-
-    //grid.setAttribute('style','grid-template-columns: repeat(range, auto);');
-
-    
  }
 
  grid.style.display="grid";
 
 }
-
-    
-
-function createSquare1(e){
-    
-   removeAllChildNodes(grid);
-  let inp= prompt("please entre the Grid size max 100!");  
-
-  console.log( e);
-  //range= e.target.valueAsNumber;
-  range= parseInt(inp);
-  console.log(typeof range);
-
-for (let index = 1; index <= range*range; index++) {
-  let sqr=document.createElement('div');
-
-  sqr.className='sqr';
-  sqr.id=`${index}`;
-
-  
-  sqr.addEventListener('click', changeColor);
-
-
-
-
-  grid.appendChild(sqr);
-  grid.style.gridTemplateColumns =`repeat(${range}, 1fr)`;
-  grid.style.gridTemplateRows =`repeat(${range}, 1fr)`;;
-
-
-  //grid.setAttribute('style','grid-template-columns: repeat(range, auto);');
-
-  
+function colorize (){
+   if (startColor =='black') {
+   this.style.background='black';
+   //Need to converst the opacity since is string
+    const opa =Number(this.style.opacity); 
+   //Add 10% of opacity for the same square once we revist it 
+   this.style.opacity=opa+ 0.1;   
 }
-
-grid.style.display="grid";
+  else if (startColor =='random')
+   this.style.background="#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();}
+    
+function changeColorSquare(choice){
+   startColor=choice;
+   
+ 
 
 }
+const colorinputblack=document.querySelector('.colorInputBlack');
+const colorinputrainBow=document.querySelector('.colorInputRainBow');
+
+colorinputblack.addEventListener('click',()=>{
+    changeColorSquare('black')});
+
+colorinputrainBow.addEventListener('mouseup',()=>{
+   changeColorSquare('random')});
+  
